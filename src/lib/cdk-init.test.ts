176496f351aa67fc28279582f720d4529520ef7d
Takashi Sato <takashi@tks.st>
2021-02-14 01:13:53 +0900

    fix import  path after the merge

--- src/lib/cdk-init.test.ts
+++ src/lib/cdk-init.test.ts
@@ -1,7 +1,7 @@
 import { expect as expectCDK, MatchStyle, matchTemplate } from "@aws-cdk/assert";
 import * as cdk from "@aws-cdk/core";
 
-import * as CdkInit from "../lib/cdk-init-stack";
+import * as CdkInit from "./cdk-init-stack";
 
 test("Empty Stack", () => {
     const app = new cdk.App();
