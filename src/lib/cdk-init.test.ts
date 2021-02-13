091d5a21f1c74ec942e65cdc62fe83ee4dfbe0bc
Takashi Sato <takashi@tks.st>
2021-02-13 23:45:27 +0900

    npm run fix after the merge

--- src/lib/cdk-init.test.ts
+++ src/lib/cdk-init.test.ts
@@ -1,13 +1,19 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as CdkInit from '../lib/cdk-init-stack';
+import { expect as expectCDK, MatchStyle, matchTemplate } from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
 
-test('Empty Stack', () => {
+import * as CdkInit from "../lib/cdk-init-stack";
+
+test("Empty Stack", () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new CdkInit.CdkInitStack(app, 'MyTestStack');
+    const stack = new CdkInit.CdkInitStack(app, "MyTestStack");
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expectCDK(stack).to(
+        matchTemplate(
+            {
+                Resources: {},
+            },
+            MatchStyle.EXACT
+        )
+    );
 });
